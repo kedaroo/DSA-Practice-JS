fcd5cecc2d6b38f38e6a00f21ed2a465fce98477
Kedar Basutkar <41827102+kedaroo@users.noreply.github.com>
2022-10-30 11:22:50 +0530

    add: shift method

--- Singly-Linked-List/app.js
+++ Singly-Linked-List/app.js
@@ -11,4 +11,19 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  // Time Complexity: O(1)
+  shift() {
+    if (this.length < 1) return;
+
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    
+    if (this.length === 1) {
+      this.tail = null;
+    }
+
+    this.length--;
+    return currentHead.value;
+  }
 }
