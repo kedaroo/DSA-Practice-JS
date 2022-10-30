64d728de5f35426602af1f8474d2a8a4049058c2
Kedar Basutkar <41827102+kedaroo@users.noreply.github.com>
2022-10-30 11:26:01 +0530

    add: unshift method

--- Singly-Linked-List/app.js
+++ Singly-Linked-List/app.js
@@ -13,12 +13,27 @@ class SinglyLinkedList {
   }
 
   // Time Complexity: O(1)
+  unshift(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+
+    this.head = newNode;
+
+    if (this.length < 1) {
+      this.tail = newNode;
+    }
+    
+    this.length++;
+    return this.length;
+  }
+
+  // Time Complexity: O(1)
   shift() {
     if (this.length < 1) return;
 
     const currentHead = this.head;
     this.head = currentHead.next;
-    
+
     if (this.length === 1) {
       this.tail = null;
     }
